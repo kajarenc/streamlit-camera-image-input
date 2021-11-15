91ffb1cc27ac4b3d5ffbeeefad72c423ee24fb98
kajarenc <kajarenc@gmail.com>
2021-11-15 21:26:06 +0400

    Fix typos

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ st.title("Webcam image input demo app!")
 image_file = st.camera_image_input()
 
 if image_file is not None:
-    x = st.slider("ROTATE TO X DEGREE", 0, 360, 0)
+    x = st.slider("ROTATE BY X DEGREE", 0, 360, 0)
 
     pil_image = Image.open(image_file)
-    rotated_iamge = pil_image.rotate(x)
-    st.image(rotated_iamge)
+    rotated_image = pil_image.rotate(x)
+    st.image(rotated_image)
